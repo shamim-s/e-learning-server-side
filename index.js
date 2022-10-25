9e49e5a446739ff41b5610c892172f161d370991
Shamim Sarkar <shamimibas@gmail.com>
2022-10-25 15:06:50 +0600

    Added some meaningfull comment

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000 ;
 
+// Called CORS here 
 app.use(cors());
 
 const courses = require('./data/data.json');
@@ -12,16 +13,19 @@ app.get('/', (req, res) => {
     res.send("E-TUTOR SERVER RUNNING");
 })
 
+//1. To get all courses form this api 
 app.get('/courses', (req, res) => {
     res.send(courses);
 })
 
+//2. To get one single course from this api
 app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
     const course = courses.find( course => course.id === id);
     res.send(course);
 })
 
+// 3. It just a duplicate api like number 2 for checkout page 
 app.get('/checkout/:id', (req, res) => {
     const id = req.params.id;
     const item= courses.find( course => course.id === id);
@@ -29,5 +33,5 @@ app.get('/checkout/:id', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`E_TUTOR SERVER RUNNING ON PORT ${port}`);
+    console.log(`E-TUTOR SERVER RUNNING ON PORT ${port}`);
 })
