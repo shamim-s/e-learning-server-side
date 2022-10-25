000ed4feb8a83d7c4e388ec7d1dca115d087437b
Shamim Sarkar <shamimibas@gmail.com>
2022-10-25 14:36:43 +0600

    Added more courses data and and search with id api

--- index.js
+++ index.js
@@ -6,10 +6,28 @@ const port = process.env.PORT || 5000 ;
 
 app.use(cors());
 
+<<<<<<< HEAD
+=======
+const courses = require('./data/data.json');
+
+>>>>>>> e7fcfa7 (Created two coursrs data and added vercel config)
 app.get('/', (req, res) => {
     res.send("E-TUTOR SERVER RUNNING");
 })
 
+<<<<<<< HEAD
+=======
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const course = courses.find( course => course.id === id);
+    res.send(course);
+})
+
+>>>>>>> e7fcfa7 (Created two coursrs data and added vercel config)
 app.listen(port, () => {
     console.log(`E_TUTOR SERVER RUNNING ON PORT ${port}`);
 })
